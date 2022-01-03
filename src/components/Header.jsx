78d7fdaa1722c6116af912988b5d171010c74dea
Natalia Camarinha <natcamarinha@gmail.com>
2022-01-03 11:56:19 -0300

    commit para portfolio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,51 +10,56 @@ function Header() {
   } = useContext(PlanetContext);
 
   return (
-    <form>
-      <label htmlFor="filter-name">
-        Pesquisa por nome:
-        <input
-          type="text"
-          data-testid="name-filter"
-          id="filter-name"
-          onChange={ handleChange }
-        />
-      </label>
-      <label htmlFor="column-filter">
-        Filtrar por categoria:
-        <select
-          name="column"
-          data-testid="column-filter"
-          id="column-filter"
-          onChange={ handleNumericValues }
-        >
-          { columnOptions.map((option, index) => (
-            <option key={ index } value={ option }>{ option }</option>))}
-        </select>
-      </label>
-      <select
-        name="comparison"
-        data-testid="comparison-filter"
-        onChange={ handleNumericValues }
-      >
-        <option>maior que</option>
-        <option>menor que</option>
-        <option>igual a</option>
-      </select>
-      <input
-        name="value"
-        type="number"
-        data-testid="value-filter"
-        onChange={ handleNumericValues }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ handleSubmit }
-      >
-        Filtrar
-      </button>
-    </form>
+    <div className="header">
+      <h2>StarWars Planets Search</h2>
+      <div>
+        <form className="form">
+          <label htmlFor="filter-name">
+            Pesquisa por nome:
+            <input
+              type="text"
+              data-testid="name-filter"
+              id="filter-name"
+              onChange={ handleChange }
+            />
+          </label>
+          <label htmlFor="column-filter">
+            Filtrar por categoria:
+            <select
+              name="column"
+              data-testid="column-filter"
+              id="column-filter"
+              onChange={ handleNumericValues }
+            >
+              { columnOptions.map((option, index) => (
+                <option key={ index } value={ option }>{ option }</option>))}
+            </select>
+          </label>
+          <select
+            name="comparison"
+            data-testid="comparison-filter"
+            onChange={ handleNumericValues }
+          >
+            <option>maior que</option>
+            <option>menor que</option>
+            <option>igual a</option>
+          </select>
+          <input
+            name="value"
+            type="number"
+            data-testid="value-filter"
+            onChange={ handleNumericValues }
+          />
+          <button
+            type="button"
+            data-testid="button-filter"
+            onClick={ handleSubmit }
+          >
+            Filtrar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
